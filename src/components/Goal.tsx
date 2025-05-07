3682809141ff9de15fe15a90b599bde7e36d370f
Eva Tokmakova <eva.tokmakova12@gmail.com>
2025-05-07 16:44:12 +0500

    changed text color for narrow screens

--- src/components/Goal.tsx
+++ src/components/Goal.tsx
@@ -10,7 +10,7 @@ export default function Goal({ svg, text }: GoalProps) {
   return (
     <div className='max-w-[481px] w-full flex items-start gap-4 '>
       <div className='w-[50px] h-[50px]'>{svg}</div>
-      <p className='leading-[14px] text-[8px] font-medium sm:text-xs md:text-sm lg:text-lg'>{text}</p>
+      <p className='leading-[14px] text-grey text-[8px] font-medium sm:text-xs md:text-sm lg:text-lg'>{text}</p>
     </div>
   );
 }
